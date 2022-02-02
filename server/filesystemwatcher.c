dc0fc77c8ff3e7d675619d2c85a12a8162343919
Frank Goyens <frankgoyens@hotmail.com>
2022-02-02 21:35:26 +0100

    Clean up the filesystem watcher a bit
    Use a correct static buffer size for reading events, according to the documentation

--- server/filesystemwatcher.c
+++ server/filesystemwatcher.c
@@ -1,13 +1,42 @@
+#include <limits.h>
 #include <sys/inotify.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-#define MAX_EVENTS 1024							  /* Maximum number of events to process*/
-#define LEN_NAME 16								  /* Assuming that the length of the filename \
-							  won't exceed 16 bytes*/
 #define EVENT_SIZE (sizeof(struct inotify_event)) /*size of one event*/
-#define BUF_LEN (MAX_EVENTS * (EVENT_SIZE + LEN_NAME))
+
+#define READING_BUFFER_INITIAL_SIZE 512
+#define READ_BUFFER_LENGTH sizeof(struct inotify_event) + NAME_MAX + 1
+
+struct ReadingBuffer{
+	char* data;
+	size_t size;
+	size_t capacity;
+};
+
+void init(struct ReadingBuffer* buffer){
+	buffer->size = 0;
+	buffer->capacity = READING_BUFFER_INITIAL_SIZE;
+	buffer->data = (char*)malloc(READING_BUFFER_INITIAL_SIZE);
+}
+
+void deinit(struct ReadingBuffer* buffer){
+	free(buffer->data);
+}
+
+void _extend(struct ReadingBuffer* buffer, size_t minimal_new_size){
+	buffer->data = (char*)realloc(buffer->data, minimal_new_size * 2);
+}
+
+void append(struct ReadingBuffer* buffer, const char* new_data, size_t new_data_size){
+	if(buffer->size + new_data_size >= buffer->capacity)
+		_extend(buffer, buffer->size + new_data_size);
+
+	memcpy(buffer->data + buffer->size, new_data, new_data_size);
+	buffer->size += new_data_size;
+}
 
 int main(int argc, char **argv)
 {
@@ -30,12 +59,12 @@ int main(int argc, char **argv)
 		printf("Watching directory %s\n", argv[1]);
 		for (;;)
 		{
-			char watch_buffer[BUF_LEN];
-			int read_length = read(inotify_fd, watch_buffer, BUF_LEN);
+			char read_buffer[READ_BUFFER_LENGTH];
+			int read_length = read(inotify_fd, read_buffer, READ_BUFFER_LENGTH);
 			int i = 0;
 			while (i < read_length)
 			{
-				struct inotify_event *event = (struct inotify_event *)&watch_buffer[i];
+				struct inotify_event *event = (struct inotify_event *)&read_buffer[i];
 				if (event->mask & IN_CREATE)
 				{
 					if (event->mask & IN_ISDIR)
