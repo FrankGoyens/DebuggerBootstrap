f2b252628b0248234e6cef71251ec1b4f93b95cf
Frank Goyens <frankgoyens@hotmail.com>
2022-02-12 21:23:16 +0100

    Handle the server and client sockets with poll

--- server/EventDispatch.c
+++ server/EventDispatch.c
@@ -11,6 +11,7 @@
 #include <sys/socket.h>
 
 enum HandleType {
+    HANDLE_TYPE_SERVER_SOCKET,
     HANDLE_TYPE_CLIENT_SOCKET,
     HANDLE_TYPE_CLIENT_SOCKET_WITH_SUBSCRIPTION // This client socket will recieve status updates as well
 };
@@ -36,8 +37,8 @@ static void Deinit(struct PollingHandles* handles) {
 static void Append(struct PollingHandles* handles, int fd, short events, enum HandleType type) {
     if (handles->size == handles->capacity) {
         handles->capacity *= 2;
-        handles->pfds = realloc(handles->pfds, handles->capacity);
-        handles->types = realloc(handles->types, handles->capacity);
+        handles->pfds = realloc(handles->pfds, handles->capacity * sizeof(struct pollfd));
+        handles->types = realloc(handles->types, handles->capacity * sizeof(enum HandleType));
         memset(handles->pfds + handles->size, 0, handles->size * sizeof(struct pollfd));
         memset(handles->types + handles->size, 0, handles->size * sizeof(enum HandleType));
     }
@@ -58,13 +59,10 @@ static void Erase(struct PollingHandles* handles, size_t at) {
     --handles->size;
 }
 
-static void StartRecievingData(int socket_desc, struct sockaddr_in* server) {
-    listen(socket_desc, 3);
-
-    printf("Waiting for incoming connections\n");
-
+static void AddClientSocket(int socket_desc, struct PollingHandles* all_handles) {
     socklen_t c = sizeof(struct sockaddr_in);
     struct sockaddr_in client;
+
     int client_sock = accept(socket_desc, (struct sockaddr*)&client, &c);
     if (client_sock < 0) {
         fprintf(stderr, "accept failed\n");
@@ -73,44 +71,72 @@ static void StartRecievingData(int socket_desc, struct sockaddr_in* server) {
 
     printf("Connection accepted\n");
 
+    Append(all_handles, client_sock, POLLIN, HANDLE_TYPE_CLIENT_SOCKET);
+
+    fcntl(client_sock, F_SETFL, fcntl(client_sock, F_GETFL, 0) | O_NONBLOCK);
+}
+
+static void StartRecievingData(int socket_desc, struct sockaddr_in* server) {
+    listen(socket_desc, 3);
+
+    printf("Waiting for incoming connections\n");
+
     struct PollingHandles all_handles;
     Init(&all_handles);
 
-    Append(&all_handles, client_sock, POLLIN, HANDLE_TYPE_CLIENT_SOCKET);
+    Append(&all_handles, socket_desc, POLLIN, HANDLE_TYPE_SERVER_SOCKET);
 
-    fcntl(client_sock, F_SETFL, fcntl(client_sock, F_GETFL, 0) | O_NONBLOCK);
+    fcntl(socket_desc, F_SETFL, fcntl(socket_desc, F_GETFL, 0) | O_NONBLOCK);
 
     size_t write_amount = 0;
     char client_message[2000];
-    size_t read_size;
-    for (;;) {
+
+    int running = 1;
+
+    while (running) {
         int ready = poll(all_handles.pfds, all_handles.size, 1000);
-        if (ready != 0) {
+        if (ready > 0) {
+            for (size_t fd_index = 0; fd_index < all_handles.size; ++fd_index) {
+                if (all_handles.pfds[fd_index].revents & POLLIN) {
+                    switch (all_handles.types[fd_index]) {
+                    case HANDLE_TYPE_SERVER_SOCKET:
+                        AddClientSocket(all_handles.pfds[fd_index].fd, &all_handles);
+                        break;
+                    case HANDLE_TYPE_CLIENT_SOCKET_WITH_SUBSCRIPTION:
+                    case HANDLE_TYPE_CLIENT_SOCKET: {
+                        size_t read_size;
 
-            if (all_handles.pfds[0].revents & POLLIN) {
+                        const int client_sock = all_handles.pfds[fd_index].fd;
 
-                read_size = recv(client_sock, client_message, 2000, 0);
+                        read_size = recv(client_sock, client_message, 2000, 0);
 
-                if (read_size > 0) {
-                    if (client_message[0] == 'q') {
-                        printf("exit requested\n");
+                        if (read_size > 0) {
+                            if (client_message[0] == 'q') {
+                                printf("exit requested\n");
+                                running = 0;
+                                break;
+                            }
+                            printf("I will write something now %lu\n", ++write_amount);
+                            write(client_sock, client_message, strlen(client_message));
+                        } else if (read_size < 0) {
+                            fprintf(stderr, "recv failed\n");
+                            Erase(&all_handles, fd_index);
+                            break;
+                        } else {
+                            printf("Client disconnected\n");
+                            Erase(&all_handles, fd_index);
+                            break;
+                        }
+
                         break;
                     }
-                    printf("I will write something now %lu\n", ++write_amount);
-                    write(client_sock, client_message, strlen(client_message));
-                } else {
-                    printf("Client disconnected\n");
-                    break;
+                    }
                 }
-
-                if (read_size == -1) {
-                    fprintf(stderr, "recv failed\n");
-                    break;
-                }
-            } else {
-                fprintf(stderr, "poll failed\n");
-                break;
             }
+
+        } else if (ready < 0) {
+            fprintf(stderr, "poll failed\n");
+            break;
         } else {
             printf("I do nothing this time %lu\n", ++write_amount);
         }
