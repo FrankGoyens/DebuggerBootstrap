ff88e0bfa41850e0ecff3d824b311a23c13ef5c3
Frank Goyens <frankgoyens@hotmail.com>
2022-02-20 22:55:00 +0100

    Formally parse arguments for server

--- server/main.c
+++ server/main.c
@@ -1,38 +1,94 @@
 #define _XOPEN_SOURCE 500
 #define _GNU_SOURCE
 
+#include <argp.h>
 #include <ftw.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "DynamicStringArray.h"
 #include "EventDispatch.h"
 
-static _Thread_local struct DynamicStringArray ftw_result_store;
+//// Argument parser stuff
+const char* argp_program_version = "DebuggerBootstrap 0.1";
+const char* argp_program_bug_address = "https://github.com/FrankGoyens/DebuggerBootstrap/issues";
 
-static int display_info(const char* fpath, const struct stat* sb, int tflag, struct FTW* ftwbuf) {
-    if (tflag == FTW_F)
-        DynamicStringArrayAppend(&ftw_result_store, fpath);
-    else if (tflag == FTW_D && ftwbuf->level > 0)
-        return FTW_SKIP_SUBTREE;
-    return FTW_CONTINUE;
+static char doc[] = "DebuggerBootstrap -- Automatically runs GDBServer when the right conditions are met.";
+
+static char args_doc[] = "[-p PORT] [--gdbserver-binary PATH]";
+
+static struct argp_option options[] = {{"verbose", 'v', 0, 0, "Produce verbose output"},
+                                       {"quiet", 'q', 0, 0, "Don't produce any output"},
+                                       {"silent", 's', 0, OPTION_ALIAS},
+                                       {"port", 'p', "PORT", 0, "Run DebuggerBootstrap at the given PORT"},
+                                       {"gdbserver-binary", 'g', "PATH", 0, "Use the GDBServer located at PATH"},
+                                       {0}};
+
+struct arguments {
+    char* gdbserver_binary;
+    int port;
+    int verbose, silent;
+};
+
+static error_t parse_opt(int key, char* arg, struct argp_state* state) {
+    /* Get the input argument from argp_parse, which we
+       know is a pointer to our arguments structure. */
+    struct arguments* arguments = state->input;
+
+    switch (key) {
+    case 'q':
+    case 's':
+        arguments->silent = 1;
+        break;
+    case 'v':
+        arguments->verbose = 1;
+        break;
+    case 'g':
+        arguments->gdbserver_binary = arg;
+        break;
+    case 'p': {
+        errno = 0;
+        arguments->port = (int)strtol(arg, NULL, 10);
+        if (errno != 0)
+            // The port could not be parsed as int
+            argp_usage(state);
+    }
+
+    case ARGP_KEY_ARG:
+        break;
+
+    case ARGP_KEY_END:
+        break;
+
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = {options, parse_opt, args_doc, doc};
+//// Argument parser stuff END
+
+void SetDefaults(struct arguments* arguments) {
+    arguments->gdbserver_binary = "/usr/bin/gdbserver";
+    arguments->port = 0;
+    arguments->silent = 0;
+    arguments->verbose = 0;
 }
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        printf("Usage: %s PATH [PATH ...]\n", argv[0]);
-        return 1;
-    }
+    struct arguments arguments;
 
-    DynamicStringArrayInit(&ftw_result_store);
-    nftw(argv[1], display_info, 1, FTW_ACTIONRETVAL);
-    for (int i = 0; i < ftw_result_store.size; ++i)
-        printf("Entry: %s\n", ftw_result_store.data[i]);
-    DynamicStringArrayDeinit(&ftw_result_store);
+    SetDefaults(&arguments);
 
-    const char* message = "hello, omg it's c time\n";
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-    // StartGDBServerProcess();
+    printf("Chosen port %d\n", arguments.port);
+    if (arguments.port == 0)
+        printf("\t(port will be determined and reported later)\n");
+    printf("GDBServer binary that will be used: %s\n", arguments.gdbserver_binary);
+
     StartEventDispatch(1337);
 
     return 0;
